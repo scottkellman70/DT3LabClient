981746b7f6d6e16fbf00716f821cd9af4474df9a
Scott Kellman <scottkellman@hotmail.com>
2020-06-29 14:50:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DT3LabClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("10.0.1.11", 8001);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(bb[i]));
+                }
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+    }
+}
